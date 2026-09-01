7c67f6c5c6b37e9509fdf298e95f783be0a52c2c
Sergii Taran <serg.taran1970@gmail.com>
2026-09-01 19:43:25 +0300

    Add global metadata and Roboto font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,20 @@
 import type { Metadata } from 'next';
+import { Roboto } from 'next/font/google';
 import type { ReactNode } from 'react';
 import { Toaster } from 'react-hot-toast';
 
-import TanStackProvider from '@/components/TanStackProvider/TanStackProvider';
-import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
+import Header from '@/components/Header/Header';
+import TanStackProvider from '@/components/TanStackProvider/TanStackProvider';
 
 import './globals.css';
+
+const roboto = Roboto({
+  weight: ['400', '500', '700'],
+  variable: '--font-roboto',
+  display: 'swap',
+  subsets: ['latin'],
+});
 
 export const metadata: Metadata = {
   title: {
@@ -15,6 +23,20 @@ export const metadata: Metadata = {
   },
   description:
     'A modern application for creating, organizing, and managing notes.',
+  openGraph: {
+    title: 'NoteHub',
+    description:
+      'A modern application for creating, organizing, and managing notes.',
+    url: 'https://notehub-app-plum.vercel.app/',
+    images: [
+      {
+        url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'NoteHub application preview',
+      },
+    ],
+  },
 };
 
 interface RootLayoutProps {
@@ -25,7 +47,7 @@ interface RootLayoutProps {
 export default function RootLayout({ children, modal }: RootLayoutProps) {
   return (
     <html lang="en">
-      <body>
+      <body className={`${roboto.className} ${roboto.variable}`}>
         <TanStackProvider>
           <Header />
           <Toaster position="top-right" />
