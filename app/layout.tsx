66f600c21a26a2782d2f0b59d77ec1099f1c54e9
Sergii Taran <serg.taran1970@gmail.com>
2026-09-01 07:54:14 +0300

    Improve root layout and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import TanStackProvider from '@/components/TanStackProvider/TanStackProvider';
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
 import { Toaster } from 'react-hot-toast';
-import './global.css';
+import './globals.css';
 
 export const metadata: Metadata = {
   title: {
