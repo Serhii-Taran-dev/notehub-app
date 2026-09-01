b2cdd153a3e9e5f6221a8de4d64da622e561d612
Sergii Taran <serg.taran1970@gmail.com>
2026-09-01 08:02:20 +0300

    Improve header accessibility

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,14 +1,14 @@
-import Link from "next/link";
-import css from "./Header.module.css";
+import Link from 'next/link';
+import css from './Header.module.css';
 
 export default function Header() {
   return (
     <header className={css.header}>
-      <Link href="/" aria-label="Home">
+      <Link href="/" aria-label="NoteHub home">
         NoteHub
       </Link>
 
-      <nav aria-label="Main Navigation">
+      <nav aria-label="Main navigation">
         <ul className={css.navigation}>
           <li>
             <Link href="/">Home</Link>
